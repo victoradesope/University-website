6121e6b4a8067ccaf403086cfee3678daa4b5a63
victor ken <victorken@gmail.com>
2024-06-23 22:17:49 +0100

    A university website code

---
+++ index.js
@@ -0,0 +1,7 @@
+let navLinks= document.getElementById("navLinks");
+        function showMenu(){
+            navLinks.style.right = "0"
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px"
+        }
